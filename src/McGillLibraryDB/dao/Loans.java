0b5d2b2ff8c8d70d8f832ffdf1d292b64496b242
Junghoon Cho <junghooncho@Junghoons-MacBook-Pro.local>
2025-03-21 11:23:26 -0400

    Added manage Loans. But need to handle copies=available in manageLoans()

--- src/McGillLibraryDB/dao/Loans.java
+++ src/McGillLibraryDB/dao/Loans.java
@@ -3,19 +3,88 @@ package McGillLibraryDB.dao;
 import java.sql.*;
 
 public class Loans {
-    private Connection connection;
+    private final Connection connection;
 
     public Loans(Connection connection) {
         this.connection = connection;
     }
 
-    public int addLoans(int card_num, String isbn, int copy_id) {
+    private int getNewLoanID(int copy_id, String isbn) {
+        String query = "SELECT MAX(loan_id) FROM loans WHERE isbn=? AND copy_id=?";
 
-        return -1;
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setString(1, isbn);
+            ps.setInt(2, copy_id);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    int maxLoanId = rs.getInt(1);
+                    return rs.wasNull() ? 1 : maxLoanId + 1;
+                }
+                return -1;
+            }
+        } catch(SQLException e) {
+            return -1;
+        }
     }
 
-    public void updateCopyStatus(String isbn, int copy_id) {
+    public int addLoans(int card_num, String isbn, int copy_id) {
+        int newLoanID = getNewLoanID(copy_id, isbn);
+        Date today = new Date(System.currentTimeMillis());
+        Date endDate = new Date(today.getTime() + 7 * 86400000L);
+        String comment;
 
+        if (!isBookAvailable(copy_id, isbn)) return 0;
+
+        String query = "INSERT INTO Loans VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setInt(1, newLoanID);
+            ps.setInt(2, copy_id);
+            ps.setString(3, isbn);
+            ps.setDate(4, today);
+            ps.setDate(5, endDate);
+            ps.setInt(6, card_num);
+
+            return ps.executeUpdate();
+        } catch (SQLException e) {
+            return -1;
+        }
+    }
+
+    private boolean isBookAvailable(int copy_id, String isbn) {
+        String query = "SELECT status FROM copies WHERE isbn=? AND copy_id=?";
+
+        try(PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setString(1, isbn);
+            ps.setInt(2, copy_id);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return (rs.getString("status").equals("available"));
+                }
+            }
+            return false;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    public int updateCopyStatus(String isbn, int copy_id) {
+        String query = "UPDATE copies SET status=? WHERE isbn=? AND copy_id=?";
+
+        try(PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setString(1, "available");
+            ps.setString(2, isbn);
+            ps.setInt(3, copy_id);
+            return ps.executeUpdate();
+
+        } catch (SQLException e) {
+            int sqlCode = e.getErrorCode();
+            String sqlStatement = e.getSQLState();
+            System.out.println(sqlCode + " " + sqlStatement);
+            System.out.println("Message: " + e.getMessage());
+            return -1;
+        }
     }
 
     public void getAllLoans() {
--- src/McGillLibraryDB/handler/LoanHandler.java
+++ src/McGillLibraryDB/handler/LoanHandler.java
@@ -16,7 +16,7 @@ public class LoanHandler {
         int copyId, status;
 
         switch (intInput) {
-            case 1:
+            case 1: // Issue a book to a customer
                 System.out.println("==== Issue a Book to a customer ====");
                 System.out.print("Enter a card number: ");
                 int cardNumber = UserInputHelper.getIntInput();
@@ -24,24 +24,31 @@ public class LoanHandler {
                 isbn = UserInputHelper.getStringInput();
                 System.out.print("Enter Book copy id: ");
                 copyId = UserInputHelper.getIntInput();
-                // Call method
+
                 status = this.loans.addLoans(cardNumber, isbn, copyId);
                 if (status > 0) {
-                    System.out.println("Successfully issued a book to " + cardNumber);
+                    // Handle copies = available.
+
+                    System.out.println("==> Successfully issued a book ( ISBN: " + isbn + ", copy_id: " + copyId + ") to " + cardNumber);
                 } else if (status == 0) {
-                    System.out.println("This book had been already issued.");
+                    System.out.println("==> (ISBN: " + isbn + ", copy_id: " + copyId + ") is not available.");
                 } else {
-                    System.out.println("Error on issuing a book to a customer. Please try again.");
+                    System.out.println("==> Error on issuing a book to a customer. Please try again.");
                 }
                 break;
-            case 2:
+            case 2: //
                 System.out.println("==== Process Book Returns ====");
                 System.out.print("Enter Book ISBN: ");
                 isbn = UserInputHelper.getStringInput();
                 System.out.print("Enter Book copy id: ");
                 copyId = UserInputHelper.getIntInput();
                 // Call method
-
+                status = this.loans.updateCopyStatus(isbn, copyId);
+                if (status > 0) {
+                    System.out.println("==> Completely returned book.");
+                } else {
+                    System.out.println("==> Error on processing a book. Please try again.");
+                }
                 break;
             case 3:
                 System.out.println("==== View Borrowing History ====");
@@ -50,7 +57,7 @@ public class LoanHandler {
             case 4:
                 break;
             default:
-                System.out.print("Invalid input, please re-enter a valid option: ");
+                System.out.print("==> Invalid input, please re-enter a valid option: ");
                 manageLoans();
                 break;
         }
